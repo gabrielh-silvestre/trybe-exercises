976f9963034d92d671007fcec9c449b4b74f3094
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-08-25 18:36:28 -0300

    create function isTriangle

--- Fundamentals/Block_04/Module_1/final_exercise/script.js
+++ Fundamentals/Block_04/Module_1/final_exercise/script.js
@@ -45,3 +45,9 @@ const biggerOneOfThree = (a, b, c) => {
 const isPositive = (a) => {
     return (a >= 0);
 }
+
+const isTriangle = (a, b, c) => {
+    if (a >= 0 && b >= 0 && c >= 0) {
+        return (a + b + c === 180);
+    }
+}
