305c045205241a2609fc9339d108a657ac2bb129
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-08-25 15:43:25 -0300

    finishing first exercise

--- Fundamentals/Block_04/Module_1/class_exercises/first_ex/scripts.js
+++ Fundamentals/Block_04/Module_1/class_exercises/first_ex/scripts.js
@@ -3,3 +3,11 @@ const birthCity = "São José do Rio Preto";
 let birthYear = 2000;
 
 console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Campinas";
+
+console.log(birthCity);
