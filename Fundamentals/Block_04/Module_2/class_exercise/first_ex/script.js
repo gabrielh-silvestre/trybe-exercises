09151581eafc88adccd04847cb9fb68cb7672e58
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-08-26 15:32:44 -0300

    first_ex part 1 correction of the method used

--- Fundamentals/Block_04/Module_2/class_exercise/first_ex/script.js
+++ Fundamentals/Block_04/Module_2/class_exercise/first_ex/script.js
@@ -1,6 +1,6 @@
 const ex1 = () => {
     let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-    let menuServices = menu.indexOf('Serviços');
+        let menuServices = menu[1];
 
     console.log(menuServices);
 }
