9f93521e9a99b6d3e6d94fcbb48d0127a48929d9
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-08-27 18:28:20 -0300

    exercise 2 solved

--- Fundamentals/Block_04/Module_3/final_exercise/script.js
+++ Fundamentals/Block_04/Module_3/final_exercise/script.js
@@ -8,3 +8,10 @@ const ex1 = (n) => {
         console.log(msg);
     }
 }
+
+const ex2 = (n) => {
+    let msg = "*";
+    for (let i = 0; i <= n; i++) {
+        console.log(msg.repeat(i));
+    }
+}
