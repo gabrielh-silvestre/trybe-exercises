304a9ed595e7ff8723480329723663451a08e958
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-08-30 20:53:16 -0300

    finishing sixth function: sum()

--- Fundamentals/Block_04/Module_4/final_exercise/function.js
+++ Fundamentals/Block_04/Module_4/final_exercise/function.js
@@ -72,4 +72,12 @@ function moreRepeats(arr) {
 
     console.log(objCompare);
 }
-moreRepeats([2, 3, 2, 5, 8, 2, 3])
+
+function sum(number) {
+    let finalSum = 0;
+    for (let i = 1; i <= number; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
+}
