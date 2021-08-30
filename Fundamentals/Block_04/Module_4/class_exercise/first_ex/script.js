77dc641296a8fa4d4bd3416e6cc7e58dc004d080
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-08-30 15:06:55 -0300

    second part of first_ex solved

--- Fundamentals/Block_04/Module_4/class_exercise/first_ex/script.js
+++ Fundamentals/Block_04/Module_4/class_exercise/first_ex/script.js
@@ -7,4 +7,6 @@ const player = {
 
 console.log(`A jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade`);
 
+player["bestInTheWorld"] = [2006, 2007, 2008, 2009, 2010, 2018];
 
+console.log(`A jogadora ${player.name} ${player.lastName} for eleita a melhor do mundo por ${player.bestInTheWorld.length} vezes`);
