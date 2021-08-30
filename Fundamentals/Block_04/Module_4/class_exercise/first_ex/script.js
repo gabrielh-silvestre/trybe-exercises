d4a5f831fe4feec7667d102715d1c8566489c357
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-08-30 15:08:40 -0300

    first_ex solved

--- Fundamentals/Block_04/Module_4/class_exercise/first_ex/script.js
+++ Fundamentals/Block_04/Module_4/class_exercise/first_ex/script.js
@@ -10,3 +10,5 @@ console.log(`A jogadora ${player.name} ${player.lastName} tem ${player.age} anos
 player["bestInTheWorld"] = [2006, 2007, 2008, 2009, 2010, 2018];
 
 console.log(`A jogadora ${player.name} ${player.lastName} for eleita a melhor do mundo por ${player.bestInTheWorld.length} vezes`);
+
+console.log(`A jogadora possui ${player.medals.golden} medalhas de ouro e ${player.medals.silver} medalhas de prata.`);
