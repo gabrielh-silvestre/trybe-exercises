96f6b0f7ae91db8c090040372bae89a3cdcb249b
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-08-30 22:55:12 -0300

    third part of bonus exercise solved

--- Fundamentals/Block_04/Module_4/final_exercise/bonus.js
+++ Fundamentals/Block_04/Module_4/final_exercise/bonus.js
@@ -13,6 +13,25 @@ const romanNumbers = {
     "CM":900,
     "M":1000
 }
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+];
 
 function romanCalc(n) {
 }
@@ -29,4 +48,23 @@ function evenSearch(arr) {
     }
 
     return evenArray;
+}
+
+function keyCounter(arr) {
+    const finalObj = {};
+
+    for (i of arr) {
+        finalObj[i] = 0;
+    }
+
+    console.log("Sua cesta possui:");
+    for (key in finalObj) {
+        for (fruit of arr) {
+            if (key === fruit) {
+                finalObj[key] += 1;
+            }
+        }
+
+        console.log(`${finalObj[key]} ${key.toLowerCase()}s;`);
+    }
 }
