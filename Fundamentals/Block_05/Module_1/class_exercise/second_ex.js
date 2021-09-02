14c72a4a30d96d70c82a0cc45e6ee399fd41a343
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-02 15:50:41 -0300

    finishing secod_ex

---
+++ Fundamentals/Block_05/Module_1/class_exercise/second_ex.js
@@ -0,0 +1,5 @@
+const allParagraphs = document.getElementsByClassName("common_paragraph");
+allParagraphs[0].style.backgroundColor = "blue";
+
+const allSubtitles = document.getElementsByClassName("subtitle");
+allSubtitles[0].style.color = "purple";
