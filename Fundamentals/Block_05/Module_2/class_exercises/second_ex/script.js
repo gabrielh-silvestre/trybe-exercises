6d68962ec1702019e881a9332e5af9b474e4540e
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-03 15:26:57 -0300

    setup second_ex

---
+++ Fundamentals/Block_05/Module_2/class_exercises/second_ex/script.js
@@ -0,0 +1,46 @@
+function accessById(id) {
+    return document.getElementById(id);
+}
+
+function getFatherElement(element) {
+    return element.parentNode;
+}
+
+function getSonElement(element) {
+    return element.children;
+}
+
+function getPreviousElement(element) {
+    return element.previousElementSibling;
+}
+
+function getNextNode(element) {
+    return element.nextElementSibling;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // req 2
+    const ondeEstou = accessById('elementoOndeVoceEsta');
+    const elementoPai = getFatherElement(ondeEstou);
+    // elementoPai.style.color = 'blue';
+    
+    // req 3
+    // const primeiroFilhoFilho = accessById('primeiroFilhoDoFilho');
+    // primeiroFilhoFilho.innerText = 'Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem'
+    
+    // req 4
+    // const pai = accessById('pai');
+    // const primeiroFilho = getSonElement(pai)[0];
+    
+    // req 5
+    // const primeiroFilho = getPreviousElement(ondeEstou);
+    
+    // req 6
+    // const atencao = getFatherElement(ondeEstou).innerText;
+    
+    // req 7
+    // const terceiroFilho = getNextNode(ondeEstou);
+
+    // req 8
+    const terceiroFilho = getSonElement(elementoPai)[2];
+})
