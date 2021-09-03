450eeb1ab205e23398a09b8f2b8edbfd7bd2a22c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-03 15:03:07 -0300

    adding notes marking the req

--- Fundamentals/Block_05/Module_2/class_exercises/first_ex/script.js
+++ Fundamentals/Block_05/Module_2/class_exercises/first_ex/script.js
@@ -6,9 +6,13 @@ function getFatherElement(element) {
     return element.parentNode;
 }
 
+// req 2
 const ondeEstou = accessById('elementoOndeVoceEsta');
 const elementoPai = getFatherElement(ondeEstou);
 elementoPai.style.color = 'blue';
 
+// req 3
 const primeiroFilhoFilho = accessById('primeiroFilhoDoFilho');
 primeiroFilhoFilho.innerText = 'Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem'
+
+// req 4
