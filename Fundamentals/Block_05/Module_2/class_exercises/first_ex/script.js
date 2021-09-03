a0e006576b3fa116492695515836bd0bd1951eb9
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-03 14:59:51 -0300

    first_ex 2 req done

--- Fundamentals/Block_05/Module_2/class_exercises/first_ex/script.js
+++ Fundamentals/Block_05/Module_2/class_exercises/first_ex/script.js
@@ -1,3 +1,11 @@
 function accessById(id) {
     return document.getElementById(id);
 }
+
+function getFatherElement(element) {
+    return element.parentNode;
+}
+
+const ondeEstou = accessById('elementoOndeVoceEsta');
+const elementoPai = getFatherElement(ondeEstou);
+elementoPai.style.color = 'blue';
