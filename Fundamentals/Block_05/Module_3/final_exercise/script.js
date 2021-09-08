77d69055d6152d7e0e062a837a334d4ac2c8c3f2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-08 15:38:39 -0300

    creation of createDaysOfDezember() function

--- Fundamentals/Block_05/Module_3/final_exercise/script.js
+++ Fundamentals/Block_05/Module_3/final_exercise/script.js
@@ -1,17 +1,17 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
   };
+};
   
-  createDaysOfTheWeek();
+createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
 
@@ -36,3 +36,16 @@ function htmlPlug(dadElement, newElement) {
 }
 
   // Exercício 1
+
+function createDaysOfDezember() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysContainer = selectingOne('#days');
+
+  dezDaysList.forEach((date) => {
+    let newDay = elementCreate('li');
+    newDay.innerText = date;
+    classAdd(newDay, 'day');
+    htmlPlug(daysContainer, newDay);
+  });
+}
+createDaysOfDezember();
