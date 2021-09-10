2bea21861f9c0b3275bc10a8d6654208b7001cbd
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-09 22:09:52 -0300

    initialization of HTML elements onload and add some eventListeners

--- Fundamentals/Block_05/Module_4/final_exercises/script.js
+++ Fundamentals/Block_05/Module_4/final_exercises/script.js
@@ -23,3 +23,25 @@ function addClass(element ,classe) {
 function removeClass(element, classe) {
     element.classList.remove(classe);
 }
+
+window.onload = () => {
+    // load HTML elements
+    const body = selectOne('body');
+    const changeBgColor = selectOne('#bg-color-selector');
+    const changeTextColor = selectOne('#text-color-selector');
+    const changeFontSize = selectOne('#font-size-selector');
+
+    changeBgColor.addEventListener('input', (event => {
+        body.style.backgroundColor = `${event.target.value}`  ;      
+    }))
+
+    changeTextColor.addEventListener('input', (event) => {
+        body.style.color = `${event.target.value}`;
+    })
+
+    changeFontSize.addEventListener('keyup', (event) => {
+        if (event.which === 13) {
+            body.style.fontSize = `${event.target.value}px`;
+        }
+    })
+}
