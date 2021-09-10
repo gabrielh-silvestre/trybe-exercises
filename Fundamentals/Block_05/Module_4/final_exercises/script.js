44735d2762344750325f01c6023aeae24bb6855c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-09 22:30:16 -0300

    application of line-height and font-family

--- Fundamentals/Block_05/Module_4/final_exercises/script.js
+++ Fundamentals/Block_05/Module_4/final_exercises/script.js
@@ -31,20 +31,45 @@ window.onload = () => {
         changeBgColor: selectOne('#bg-color-selector'),
         changeTextColor: selectOne('#text-color-selector'),
         changeFontSize: selectOne('#font-size-selector'),
-        changeLineHeight: selectOne('#line-height-selector')
+        changeLineHeight: selectOne('#line-height-selector'),
+        changeFont: selectAll('.btn-customize')
     }
 
-    changeElements.changeBgColor.addEventListener('input', (event => {
-        body.style.backgroundColor = `${event.target.value}`  ;      
-    }))
+    changeElements.changeBgColor.addEventListener('input', (event) => {
+        body.style.backgroundColor = `${event.target.value}`;
+    });
 
     changeElements.changeTextColor.addEventListener('input', (event) => {
         body.style.color = `${event.target.value}`;
-    })
+    });
 
     changeElements.changeFontSize.addEventListener('keyup', (event) => {
         if (event.which === 13) {
             body.style.fontSize = `${event.target.value}px`;
         }
+    });
+
+    changeElements.changeLineHeight.addEventListener('keyup', (event) => {
+        if (event.which === 13) {
+            body.style.lineHeight = `${event.target.value}px`;
+        }
+    });
+
+    changeElements.changeFont.forEach((fontOption) => {
+        fontOption.addEventListener('click', (event) => {
+            switch(event.target.innerText) {
+                case 'Arial':
+                    body.style.fontFamily = 'Arial';
+                    break
+                case 'Georgia':
+                    body.style.fontFamily = 'Georgia';
+                    break
+                case 'monospace':
+                    body.style.fontFamily = 'monospace';
+                    break
+                default:
+                    body.style.fontFamily = '"Times New Roman"';
+            }
+        })
     })
 }
