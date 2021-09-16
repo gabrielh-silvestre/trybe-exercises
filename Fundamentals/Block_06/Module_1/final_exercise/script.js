c1da54b22a793ec62137b08fcea4a6fb3d457948
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-15 22:13:20 -0300

    creation of generateStates function

--- Fundamentals/Block_06/Module_1/final_exercise/script.js
+++ Fundamentals/Block_06/Module_1/final_exercise/script.js
@@ -8,6 +8,8 @@ Caso todos os dados sejam válidos, monte uma <div> com o consolidado dos dados 
 
 Caso haja algum dado inválido, mostre em uma <div> uma mensagem de erro. Se o erro for na Data de Início , a mensagem deve ser contextualizada. */
 
+// http://127.0.0.1:5500/index.html?user-name=Ga&user-email=E%40E&user-cpf=33388811100&user-city=Cidade&user-home=on&user-resume=abalacabaca+abalacabaca+abalacabaca+abalacabaca+abalacabaca+abalacabaca+&user-job=foda&user-job-description=muito+foda&user-start-date=26%2F08%2F2010#
+
 const user = {
   userName: '',
   userEmail: '',
@@ -20,3 +22,24 @@ const user = {
   userJobDesction: '',
   userStartDate: '',
 }
+
+const staticElement = {
+  inputState: getOne('#input-state'),
+}
+
+const brazilStates = ['Acre','Alagoas','Amazonas','Amapa','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Minas Gerais','Mato Grosso do Sul','Mato Grosso','Pará','Paraíba','Pernambuco','Piauí','Paraná','Rio de Janeiro','Rio Grande do Norte','Rondônia','Roraima','Rio Grande do Sul','Santa Catarina','Sergipe','São Paulo','Tocantins'];
+const stateInitials = ['AC','AL','AM','AP','BA','CE','DF','ES','GO','MA','MG','MS','MT','PA','PB','PE','PI','PR','RJ','RN','RO','RR','RS','SC','SE','SP','TO'];
+
+function generateStates(stateInitialsArray, stateArray) {
+  console.log(stateInitials);
+  stateArray.forEach((state, i) => {
+    const stateOption = createElement('option');
+    stateOption.innerText = state;
+    stateOption.value = stateInitialsArray[i];
+    plugHtml(staticElement.inputState, stateOption);
+  });
+}
+
+window.onload = () => {
+  generateStates(stateInitials, brazilStates);
+};
