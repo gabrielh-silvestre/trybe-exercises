176f1ac3f2a9b2450fbc26ecde8289b7e7640497
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-15 21:36:07 -0300

    transcript exercises and make user obj

---
+++ Fundamentals/Block_06/Module_1/final_exercise/script.js
@@ -0,0 +1,22 @@
+/* Logo abaixo do formulário, crie um botão que:
+
+Chame uma função JavaScript e interrompa o fluxo automático do form utilizando o preventDefault() . Note que isso vai impedir as validações do HTML ao fazer o submit
+
+Implemente, agora, no Javascript , as validações que foram pedidas ao longo da montagem do formulário.
+
+Caso todos os dados sejam válidos, monte uma <div> com o consolidado dos dados que foram inseridos no formulário.
+
+Caso haja algum dado inválido, mostre em uma <div> uma mensagem de erro. Se o erro for na Data de Início , a mensagem deve ser contextualizada. */
+
+const user = {
+  userName: '',
+  userEmail: '',
+  userCpf: '',
+  userCity: '',
+  userState: '',
+  userHome: '',
+  userResume: '',
+  userJob: '',
+  userJobDesction: '',
+  userStartDate: '',
+}
