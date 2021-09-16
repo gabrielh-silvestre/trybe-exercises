91547b889f092ab1b67325076eefe2f453ca4ccc
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-16 17:31:59 -0300

    adding more functions

--- Fundamentals/Block_06/Module_1/final_exercise/my_functions.js
+++ Fundamentals/Block_06/Module_1/final_exercise/my_functions.js
@@ -33,3 +33,19 @@ function addMultiplesEvents(element, eventsName, listener) {
     element.addEventListener(event, listener, false);
   });
 }
+
+function addMultiplesListeners(arr, eventName, listener) {
+  arr.forEach((element) => {
+    element.addEventListener(eventName, listener, false);
+  });
+}
+
+function addMultiplesEventsAndListeners(arr, eventsName, listener) {
+  const events = eventsName.split(' ');
+
+  arr.forEach((element) => {
+    events.forEach((event) => {
+      element.addEventListener(event, listener, false);
+    });
+  });
+}
