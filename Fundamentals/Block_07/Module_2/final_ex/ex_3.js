585b6e1c100e38a5a299867c95f5919b267c3c88
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-24 13:26:45 -0300

    third exercise done

--- Fundamentals/Block_07/Module_2/final_ex/ex_3.js
+++ Fundamentals/Block_07/Module_2/final_ex/ex_3.js
@@ -18,6 +18,8 @@ const lesson3 = {
   turno: 'noite',
 };
 
+const lessonsArr = [lesson1, lesson2, lesson3];
+
 const allLesson = (l1, l2, l3) => { 
   const compactedLessons = {
     lesson1: {},
@@ -31,3 +33,33 @@ const allLesson = (l1, l2, l3) => {
 
   return compactedLessons;
 };
+
+const lessonViwers = (obj, subject) => {
+  const lessonsArr = Object.entries(obj);
+  let viwers = 0;
+
+  lessonsArr.forEach((lesson) => {
+    if (lesson[1]['materia'] === subject) {
+      viwers += lesson[1]['numeroEstudantes'];
+    }
+  });
+
+  return viwers;
+};
+// console.log(lessonViwers(allLesson(...lessonsArr), 'Matemática'));
+
+const createReport = (obj, professor) => {
+  const lessonsArr = Object.entries(obj);
+  const finalReport = {professor: '', aulas: [], estudantes: 0,};
+
+  lessonsArr.forEach((lesson) => {
+    if (lesson[1]['professor'] === professor) {
+      finalReport.professor = lesson[1]['professor'];
+      finalReport.aulas.push(lesson[1]['materia']);
+      finalReport.estudantes += lesson[1]['numeroEstudantes'];
+    }
+  });
+
+  return finalReport;
+};
+// console.log(createReport(allLesson(...lessonsArr), 'Maria Clara'));
