dcec26abbaa5a3aae4e2d804074f2e9410f8a764
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-28 15:30:30 -0300

    reducing hydrate function

--- Fundamentals/Block_07/Module_3/final_exercises/hydrate.js
+++ Fundamentals/Block_07/Module_3/final_exercises/hydrate.js
@@ -1,7 +1,8 @@
 function hydrate(str) {
   const cupsArray = str.split('');
-  const numberArray = cupsArray.filter((n) => Number.parseInt(n, 10)).map((n) => Number.parseInt(n, 10));
-  const sum = numberArray.reduce((prevNumber, number) => prevNumber + number)
+  const sum = cupsArray.filter((n) => Number.parseInt(n, 10))
+  .map((n) => Number.parseInt(n, 10))
+  .reduce((prevNumber, number) => prevNumber + number)
 
   return sum > 1 ? `${sum} copos de água` : `${sum} copo de água`;
 }
