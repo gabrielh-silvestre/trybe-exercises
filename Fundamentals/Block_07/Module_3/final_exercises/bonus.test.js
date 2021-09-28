d880d5e0e5e1666fc643b91ef490c0a08ba504d9
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-28 15:55:57 -0300

    implement and solving 3 test

---
+++ Fundamentals/Block_07/Module_3/final_exercises/bonus.test.js
@@ -0,0 +1,33 @@
+const searchEmployee = require('./bonus');
+
+describe('Test for bonus exercise', () => {
+  
+  const testEmployees = [
+    {
+      id: '9852-2-2',
+      firstName: 'Jeff',
+      lastName: 'Cook',
+      specialities: ['Ruby', 'SQL'],
+    },
+    {
+      id: '4678-2',
+      firstName: 'Paul',
+      lastName: 'Dodds',
+      specialities: ['Backend'],
+    },
+  ]
+
+  it('Verify if searchEmployee is declared', () => {
+    expect(searchEmployee).toBeDefined();
+  });
+
+  it('Verify if searchEmployee is a function', () => {
+    expect(typeof searchEmployee).toBe('function');
+  });
+
+  it('Verify if searchEmployee return a existing employee by id', () => {
+    expect(searchEmployee('9852-2-2')).not.toBeUndefined();
+    expect(typeof searchEmployee('9852-2-2')).toBe('object');
+  });
+
+});
