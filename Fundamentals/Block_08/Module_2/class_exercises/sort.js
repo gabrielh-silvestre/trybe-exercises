46362501a469b6f332e751825ce63bf9ec7a6f9a
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-05 18:39:53 -0300

    fourth class exercise done

---
+++ Fundamentals/Block_08/Module_2/class_exercises/sort.js
@@ -0,0 +1,15 @@
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people.sort((a, b) => a.age - b.age); // crescente
+
+console.log(people);
+
+people.sort((a, b) => b.age - a.age); // decrescente
+
+console.log(people);
