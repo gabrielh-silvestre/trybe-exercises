58cebbcb7c75eaa22cc40983571c28bd9181808c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-05 19:05:37 -0300

    implement someBookWasReleaseOnThe80s function

--- Fundamentals/Block_08/Module_2/final_exercises/script.js
+++ Fundamentals/Block_08/Module_2/final_exercises/script.js
@@ -154,3 +154,11 @@ function everyoneWasBornOnSecXX() {
 }
 
 assert.strictEqual(everyoneWasBornOnSecXX(), expectedResultBornOnSecXX);
+
+const expectedResult = true;
+
+function someBookWasReleaseOnThe80s() {
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear <= 1989);
+}
+
+assert.strictEqual(someBookWasReleaseOnThe80s(), expectedResult);
