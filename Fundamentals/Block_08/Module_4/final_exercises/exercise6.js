dd7f9e5fc30858cc9f1678e39d4b8d3e363780b8
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-07 18:08:42 -0300

    finishing ex 6

--- Fundamentals/Block_08/Module_4/final_exercises/exercise6.js
+++ Fundamentals/Block_08/Module_4/final_exercises/exercise6.js
@@ -1,8 +1,15 @@
 const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
-function studentAverage() {
-  // escreva seu código aqui
+const getAverage = (notes) => notes.reduce((acc, curr) => acc + curr) / notes.length;
+
+function studentAverage(students, grades) {
+  return students.map((student, i) => {
+    return {
+      name: student,
+      average: getAverage(grades[i]),
+    }
+  });
 }
 
 const expected = [
