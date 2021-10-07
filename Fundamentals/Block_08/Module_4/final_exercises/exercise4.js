06a25b90d90ce1e0537afcbba3c7874b840e9232
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-07 17:30:15 -0300

    starting ex 4

---
+++ Fundamentals/Block_08/Module_4/final_exercises/exercise4.js
@@ -0,0 +1,16 @@
+const books = require("./template");
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+function longestNamedBook() {
+
+}
