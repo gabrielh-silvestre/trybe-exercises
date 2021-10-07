0248559fc6e25d1667d7c737ccdf752853215e5c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-07 18:01:33 -0300

    finishing ex 5

--- Fundamentals/Block_08/Module_4/final_exercises/exercise5.js
+++ Fundamentals/Block_08/Module_4/final_exercises/exercise5.js
@@ -4,6 +4,15 @@ const names = [
   'Abeladerco', 'Adieidy', 'Alarucha',
 ];
 
-function containsA() {
-  // escreva seu código aqui
+const splitLetters = (str) => str.split('');
+
+const countA = (arr) => arr.filter((l) => l.toLowerCase() === 'a').length;
+
+function containsA(arr) {
+  return arr
+    .map((name) => splitLetters(name))
+    .map((arrName) => countA(arrName))
+    .reduce((acc, curr) => acc + curr);
 }
+
+console.log(containsA(names));
