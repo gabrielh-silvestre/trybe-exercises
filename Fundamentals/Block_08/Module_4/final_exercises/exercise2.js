868aa24ccc925d8596cdf7153d3c104be2c49e45
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-07 17:21:13 -0300

    finishing ex 2

--- Fundamentals/Block_08/Module_4/final_exercises/exercise2.js
+++ Fundamentals/Block_08/Module_4/final_exercises/exercise2.js
@@ -1,5 +1,8 @@
+const books = require('./template');
+
 const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
 
-function reduceNames() {
-  // escreva seu código aqui
+function reduceNames(books) {
+  return books.map((book) => book.author.name)
+    .reduce((acc, curr) => acc.concat(', ', curr));
 }
