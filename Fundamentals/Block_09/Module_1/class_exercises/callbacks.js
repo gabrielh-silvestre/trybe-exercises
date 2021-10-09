397571156db71cdcdc656b5eb08267192d72efd2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-09 17:31:13 -0300

    first ex of callbacks.js done

--- Fundamentals/Block_09/Module_1/class_exercises/callbacks.js
+++ Fundamentals/Block_09/Module_1/class_exercises/callbacks.js
@@ -2,14 +2,16 @@ const exercise1 = () => {
   const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
   const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-  const getUser = () => {
+  const getUser = (callback) => {
     const userToReturn = {
       firstName: "Ivan",
       lastName: "Ivanovich",
       nationality: "Russian"
     };
+
+    return callback(userToReturn)
   };
 
-  console.log(getUser()); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
-  console.log(getUser()); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+  console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+  console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
 }
