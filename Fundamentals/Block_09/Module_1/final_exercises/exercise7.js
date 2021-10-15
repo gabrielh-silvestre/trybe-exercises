bb185e46bf907b1ab28043eb65efc9f48ad50f7d
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-15 15:56:00 -0300

    exercise 7 done

--- Fundamentals/Block_09/Module_1/final_exercises/exercise7.js
+++ Fundamentals/Block_09/Module_1/final_exercises/exercise7.js
@@ -3,3 +3,14 @@ const uppercase = (str, callback) => {
     callback(str.toUpperCase());
   }, 500);
 };
+
+it('Check if the strings returns all uppercase', (done) => {
+  uppercase('test', (str) => {
+    try {
+      expect(str).toBe('TEST');
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
