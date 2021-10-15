24346988a348fc2f6027dd53979f95a4a4322ef4
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-15 16:40:55 -0300

    exercise 9 done

--- Fundamentals/Block_09/Module_1/final_exercises/exercise8.test.js
+++ Fundamentals/Block_09/Module_1/final_exercises/exercise8.test.js
@@ -1,12 +1,19 @@
 // Verifique se a importação do arquivo correto está sendo feita.
-const { getPokemonDetails } = require("./get-pokemon-details");
+const { getPokemonDetails } = require("./exercise8");
 
+const pokeFilter = (pokemon) => pokemon.name === 'Squirtl';
 describe("A função getPokemonDetails", () => {
   it("retorna erro quando procuramos um pokemon que não existe no banco de dados", (done) => {
-    // Escreva aqui seu código
+    const expectError = new Error('Não temos esse pokémon para você :(');
+
+    expect(getPokemonDetails(pokeFilter, (error, msg) => expect(error).toEqual(expectError)));
+    done();
   });
 
   it("retorna um pokemon que existe no banco de dados", (done) => {
-    // Escreva aqui seu código
+    const expectedPokemon = 'Olá, seu pokémon é o Squirtle, o tipo dele é Water e a habilidade principal dele é Water Gun';
+
+    expect(getPokemonDetails(pokeFilter, (error, msg) => expect(msg).toEqual(expectedPokemon)));;
+    done();
   });
 });
