cbcc13c9ec3461258b4bc7a135984c8d0dd0a368
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-19 15:36:55 -0300

    starting exercise 1

---
+++ Fundamentals/Block_09/Module_3/exercise1/ex1.js
@@ -0,0 +1,13 @@
+// githubApi.test.js
+
+const fetch = require('node-fetch');
+
+const getRepos = (url) => (
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.map((repo) => repo.name))
+);
+
+module.exports = getRepos;
+
+// implemente seus testes aqui
---
+++ Fundamentals/Block_09/Module_3/exercise1/ex1.test.js
@@ -0,0 +1,1 @@
+const getRepos = require('./ex1');
