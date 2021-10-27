4812f266ba8171e95b424c24127bb6d462a95dd0
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-27 19:05:10 -0300

    feat: import and render Content component

--- Front-End/Block_01/final_exercises/day-10.1-final-ex/src/App.js
+++ Front-End/Block_01/final_exercises/day-10.1-final-ex/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 // import TaskList from './components/Exercise1/TaskList';
 import Header from './components/Exercise2/Header';
+import Content from './components/Exercise2/Content';
 
 // const myTasks = ['Task1', 'Task2', 'Task3', 'Task4', 'Task5'];
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Content />
       {/* <TaskList tasks={myTasks} /> */}
     </div>
   );
