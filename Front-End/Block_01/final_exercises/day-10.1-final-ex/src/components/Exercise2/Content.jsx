9155e6d7a77f2552f0cd9fcd792768c9fa77dcf4
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-27 19:00:06 -0300

    feat: implement content render in Content component

--- Front-End/Block_01/final_exercises/day-10.1-final-ex/src/components/Exercise2/Content.jsx
+++ Front-End/Block_01/final_exercises/day-10.1-final-ex/src/components/Exercise2/Content.jsx
@@ -1,11 +1,54 @@
 import React, { Component } from 'react'
 
 export default class Content extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      conteudos: [
+        {
+          conteudo: 'High Order Functions',
+          bloco: 8,
+          status: 'Aprendido'
+        },
+        {
+          conteudo: 'Composicao de Componentes',
+          bloco: 11,
+          status: 'Aprendendo',
+        },
+        {
+          conteudo: 'Composicao de Estados',
+          bloco: 12,
+          status: 'Aprenderei'
+        },
+        {
+          conteudo: 'Redux',
+          bloco: 16,
+          status: 'Aprenderei'
+        },
+      ]
+    }
+
+    this.renderContent = this.renderContent.bind(this);
+  }
+
+  renderContent() {
+    const { consteudos } = this.state;
+
+    consteudos.map(({ conteudo, bloco, status }) => (
+      <section>
+        <p>O conteúdo é: {conteudo}</p>
+        <p>Status: {status}</p>
+        <p>Bloco: {bloco}</p>
+      </section>
+    ))
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <>
+        {this.renderContent()}
+      </>
     )
   }
 }
