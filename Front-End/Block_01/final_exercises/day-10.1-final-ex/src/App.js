bc66e5ba7b50d75e3da08d7a8cbd62c655e70c75
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-27 18:12:09 -0300

    feat: import and render TaskList on App

--- Front-End/Block_01/final_exercises/day-10.1-final-ex/src/App.js
+++ Front-End/Block_01/final_exercises/day-10.1-final-ex/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import TaskList from './components/Exercise1/TaskList';
+
+const myTasks = ['Task1', 'Task2', 'Task3', 'Task4', 'Task5'];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TaskList tasks={myTasks} />
     </div>
   );
 }
