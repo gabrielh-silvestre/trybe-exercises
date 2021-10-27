54c0957dbba31617378efc7bef147924de9d9427
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-27 19:07:45 -0300

    feat: import and render Footer component

--- Front-End/Block_01/final_exercises/day-10.1-final-ex/src/App.js
+++ Front-End/Block_01/final_exercises/day-10.1-final-ex/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import TaskList from './components/Exercise1/TaskList';
 import Header from './components/Exercise2/Header';
 import Content from './components/Exercise2/Content';
+import Footer from './components/Exercise2/Footer';
 
 // const myTasks = ['Task1', 'Task2', 'Task3', 'Task4', 'Task5'];
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <Content />
+      <Footer />
       {/* <TaskList tasks={myTasks} /> */}
     </div>
   );
