93caa0b214f36cbff87e428a3d08765fcd14f25b
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-27 19:07:05 -0300

    feat: implement Footer component content

--- Front-End/Block_01/final_exercises/day-10.1-final-ex/src/components/Exercise2/Footer.jsx
+++ Front-End/Block_01/final_exercises/day-10.1-final-ex/src/components/Exercise2/Footer.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <footer>
+        <h1>E isso é só o começo...</h1>
+      </footer>
     )
   }
 }
