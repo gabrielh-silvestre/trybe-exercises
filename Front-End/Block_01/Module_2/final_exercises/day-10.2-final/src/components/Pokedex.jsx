151f71192d3ddb4e192c3b567b8020392c2b6294
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-28 17:43:21 -0300

    style: customize Pokemon and Pokedex

--- Front-End/Block_01/Module_2/final_exercises/day-10.2-final/src/components/Pokedex.jsx
+++ Front-End/Block_01/Module_2/final_exercises/day-10.2-final/src/components/Pokedex.jsx
@@ -5,7 +5,7 @@ import pokemons from '../data/data';
 export default class Pokedex extends Component {
   render() {
     return (
-      <article>
+      <article className="flex flex-wrap justify-between w-11/12 mx-auto">
         {pokemons.map((poke) => <Pokemon key={poke.name} pokemon={poke} />)}
       </article>
     )
--- Front-End/Block_01/Module_2/final_exercises/day-10.2-final/src/components/Pokemon.jsx
+++ Front-End/Block_01/Module_2/final_exercises/day-10.2-final/src/components/Pokemon.jsx
@@ -1,21 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Pokemon extends Component {
   render() {
     const { name, type, averageWeight, image } = this.props.pokemon;
 
     return (
-      <section>
-        <div>
-          <p>{name}</p>
-          <p>{type}</p>
-          <p>{`${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
-        </div>
+      <section className="inline-block m-4 py-2 px-8 shadow-md rounded-xl bg-gray-50">
+        <div className="flex justify-between items-center">
+          <div className="w-20">
+            <p>{name}</p>
+            <p>{type}</p>
+            <p>{`${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+          </div>
 
-        <div>
-          <img src={image} alt={name} />
+          <div>
+            <img src={image} alt={name} />
+          </div>
         </div>
       </section>
-    )
+    );
   }
 }
