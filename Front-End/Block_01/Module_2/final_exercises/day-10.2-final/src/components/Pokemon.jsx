03b29189ef0e2db32f2bef91ec68fd02d8893078
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-28 16:55:27 -0300

    feat(Pokemon): implement component structure

--- Front-End/Block_01/Module_2/final_exercises/day-10.2-final/src/components/Pokemon.jsx
+++ Front-End/Block_01/Module_2/final_exercises/day-10.2-final/src/components/Pokemon.jsx
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 
 export default class Pokemon extends Component {
   render() {
+    const { name, type, averageWeight, image } = this.props.pokemon;
+
     return (
-      <div>
-        
-      </div>
+      <section>
+        <div>
+          <p>{name}</p>
+          <p>{type}</p>
+          <p>{`${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+        </div>
+
+        <div>
+          <img src={image} alt={name} />
+        </div>
+      </section>
     )
   }
 }
