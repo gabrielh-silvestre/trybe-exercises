d880168812ceccfb9586bd4dc85c14855b5a6cc2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-10-28 15:51:33 -0300

    feat: render Order component

--- Front-End/Block_01/Module_2/class_exercises/day-10.2-class/src/App.js
+++ Front-End/Block_01/Module_2/class_exercises/day-10.2-class/src/App.js
@@ -26,7 +26,8 @@ function App() {
   return (
     <div className="App">
       <h1> Orders recently created </h1>
-
+      <Order order={headphone} />
+      <Order order={energyDrink} />
       {/* <Image img="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" /> */}
     </div>
   );
