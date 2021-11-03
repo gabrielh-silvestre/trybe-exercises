5277814de683c1394705bdecde8674adbd3eedc8
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-03 17:45:02 -0300

    feat(Pokedex): implement diable button when do not have next pokemon

--- Front-End/Block_02/final_exercise/src/components/Pokedex.js
+++ Front-End/Block_02/final_exercise/src/components/Pokedex.js
@@ -7,8 +7,10 @@ class Pokedex extends React.Component {
     super(props);
 
     this.state = {
+      filteredPokemons: this.props.pokemons,
       index: 0,
       filter: undefined,
+      isAble: false,
     };
 
     this.nextPokemon = this.nextPokemon.bind(this);
@@ -17,10 +19,10 @@ class Pokedex extends React.Component {
   }
 
   nextPokemon() {
-    const pokemons = this.filterByType();
+    const { filteredPokemons } = this.state;
 
     this.setState((prevState) => {
-      return prevState.index === pokemons.length - 1
+      return prevState.index === filteredPokemons.length - 1
         ? { index: 0 }
         : { index: prevState.index + 1 };
     });
@@ -31,27 +33,42 @@ class Pokedex extends React.Component {
     const { pokemons } = this.props;
 
     if (filter) {
-      return pokemons.filter((p) => p.type === filter);
+      this.setState({
+        filteredPokemons: pokemons.filter((p) => p.type === filter),
+        isAble: pokemons.filter((p) => p.type === filter).length === 1,
+      });
+    } else {
+      this.setState({ filteredPokemons: this.props.pokemons, isAble: false });
     }
-    return pokemons;
   }
 
   setFilter({ target }) {
-    this.setState((prevState) => {
-      if (prevState.filter === target.innerText) {
-        return { filter: undefined, index: 0 };
-      }
-      return { filter: target.innerText, index: 0 };
-    });
+    this.setState((prevState) =>
+      prevState.filter === target.innerText
+        ? { filter: undefined, index: 0 }
+        : { filter: target.innerText, index: 0 }
+    );
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { filter } = this.state;
+
+    if (prevState.filter !== filter) {
+      this.filterByType();
+    }
   }
 
   render() {
-    const { index } = this.state;
+    const { index, isAble, filteredPokemons } = this.state;
 
     return (
       <div className="pokedex">
-        <Pokemon pokemon={this.filterByType()[index]} />
-        <Button callback={this.nextPokemon} btnText="Próximo Pokemon" />
+        <Pokemon pokemon={filteredPokemons[index]} />
+        <Button
+          able={isAble}
+          callback={this.nextPokemon}
+          btnText="Próximo Pokemon"
+        />
 
         <div>
           <Button callback={this.setFilter} btnText="Electric" />
