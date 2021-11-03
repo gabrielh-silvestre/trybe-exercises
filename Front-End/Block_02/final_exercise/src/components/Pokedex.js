711f136ce4bded2c1b2e0c280d493b8795b03e61
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-03 16:55:17 -0300

    feat(Pokedex): implement filter by type

--- Front-End/Block_02/final_exercise/src/components/Pokedex.js
+++ Front-End/Block_02/final_exercise/src/components/Pokedex.js
@@ -6,13 +6,18 @@ class Pokedex extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { index: 0 };
+    this.state = {
+      index: 0,
+      filter: undefined,
+    };
 
     this.nextPokemon = this.nextPokemon.bind(this);
+    this.filterByType = this.filterByType.bind(this);
+    this.setFilter = this.setFilter.bind(this);
   }
 
   nextPokemon() {
-    const { pokemons } = this.props;
+    const pokemons = this.filterByType();
 
     this.setState((prevState) => {
       return prevState.index === pokemons.length - 1
@@ -21,14 +26,40 @@ class Pokedex extends React.Component {
     });
   }
 
-  render() {
+  filterByType() {
+    const { filter } = this.state;
     const { pokemons } = this.props;
+
+    if (filter) {
+      return pokemons.filter((p) => p.type === filter);
+    }
+    return pokemons;
+  }
+
+  setFilter({ target }) {
+    this.setState((prevState) => {
+      if (prevState.filter === target.innerText) {
+        return { filter: undefined, index: 0 };
+      }
+      return { filter: target.innerText, index: 0 };
+    });
+  }
+
+  render() {
     const { index } = this.state;
 
     return (
       <div className="pokedex">
-        <Pokemon pokemon={pokemons[index]} />
+        <Pokemon pokemon={this.filterByType()[index]} />
         <Button callback={this.nextPokemon} btnText="Próximo Pokemon" />
+
+        <div>
+          <Button callback={this.setFilter} btnText="Electric" />
+          <Button callback={this.setFilter} btnText="Fire" />
+          <Button callback={this.setFilter} btnText="Bug" />
+          <Button callback={this.setFilter} btnText="Normal" />
+          <Button callback={this.setFilter} btnText="Psychic" />
+        </div>
       </div>
     );
   }
