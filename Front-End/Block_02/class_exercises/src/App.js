4ce61ecd49d214ca2064bf245df84c5bf3fabc83
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-03 15:15:16 -0300

    feat(App.js): import Exercise3

--- Front-End/Block_02/class_exercises/src/App.js
+++ Front-End/Block_02/class_exercises/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 // import Exercise1 from './Exercise1/Exercise1';
-import Exercise2 from './Exercise2/Exercise2';
+// import Exercise2 from './Exercise2/Exercise2';
+import Exercise3 from './Exercise3/Exercise3';
 
 function App() {
   return (
     <div className="App">
       {/* <Exercise1 text="trem" /> */}
-      <Exercise2 />
+      {/* <Exercise2 /> */}
+      <Exercise3 />
     </div>
   );
 }
