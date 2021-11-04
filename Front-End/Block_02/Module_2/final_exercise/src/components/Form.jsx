9a0b2cceb84403b586cba8c9e41cf975aacb93fb
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 18:25:04 -0300

    feat(Form): implement first Fieldset

--- Front-End/Block_02/Module_2/final_exercise/src/components/Form.jsx
+++ Front-End/Block_02/Module_2/final_exercise/src/components/Form.jsx
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Fieldset from './Fieldset';
+import Input from './Input';
+import Select from './Select';
 
 export default class Form extends Component {
   render() {
     return (
       <form>
-        
+        <Fieldset id="personal-data">
+          <Input type="text" name="name" handleChange="" />
+          <Input type="email" name="email" handleChange="" />
+          <Input type="number" name="cpf" handleChange="" />
+          <Input type="text" name="addres" handleChange="" />
+          <Input type="text" name="city" handleChange="" />
+          <Select name="state" handleChange="" options={[]} />
+          <div>
+            <input type="radio" name="home" value="house" checked={true} />
+            <input type="radio" name="home" value="appartment" />
+          </div>
+        </Fieldset>
       </form>
-    )
+    );
   }
 }
