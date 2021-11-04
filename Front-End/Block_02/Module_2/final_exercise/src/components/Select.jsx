df5f6b4f475ebeb4077f12eaf9f592f5833bd306
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 18:12:46 -0300

    feat(Select): implement dinamic options genration

--- Front-End/Block_02/Module_2/final_exercise/src/components/Select.jsx
+++ Front-End/Block_02/Module_2/final_exercise/src/components/Select.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Select extends Component {
   render() {
+    const { name, options } = this.props;
+
     return (
-      <select>
-        
+      <select id={name}>
+        {options.map((opt) => (
+          <option key={opt} value={opt}>
+            {opt}
+          </option>
+        ))}
       </select>
-    )
+    );
   }
 }
