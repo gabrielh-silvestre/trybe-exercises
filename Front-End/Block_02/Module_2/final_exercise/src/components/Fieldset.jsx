d20b2c30767465dc77e6220a4cbb2894bec17cb8
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 18:05:48 -0300

    feat(Fieldset): render component childrens

--- Front-End/Block_02/Module_2/final_exercise/src/components/Fieldset.jsx
+++ Front-End/Block_02/Module_2/final_exercise/src/components/Fieldset.jsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react'
 
 export default class Fieldset extends Component {
   render() {
+    const { children, id } = this.props;
+
     return (
-      <fieldset>
-        
+      <fieldset id={id}>
+        {children}
       </fieldset>
     )
   }
