beca2b34e8dc0e0fb59baa425b61c25c8eb3bae4
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 18:35:40 -0300

    feat(Form): add label on radio buttons

--- Front-End/Block_02/Module_2/final_exercise/src/components/Form.jsx
+++ Front-End/Block_02/Module_2/final_exercise/src/components/Form.jsx
@@ -4,6 +4,19 @@ import Input from './Input';
 import Select from './Select';
 
 export default class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: undefined,
+      email: undefined,
+      cpf: undefined,
+      addres: undefined,
+      city: undefined,
+      home: 'house',
+    };
+  }
+
   render() {
     return (
       <form>
@@ -15,8 +28,21 @@ export default class Form extends Component {
           <Input type="text" name="city" handleChange="" />
           <Select name="state" handleChange="" options={[]} />
           <div>
-            <input type="radio" name="home" value="house" checked={true} />
-            <input type="radio" name="home" value="appartment" />
+            <label htmlFor="house">House</label>
+            <input
+              type="radio"
+              name="home"
+              value="house"
+              id="house"
+              checked={true}
+            />
+            <label htmlFor="appartment">Appartment</label>
+            <input
+              type="radio"
+              name="home"
+              value="appartment"
+              id="appartment"
+            />
           </div>
         </Fieldset>
       </form>
