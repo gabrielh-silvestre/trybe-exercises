f7c5cfd8ea2c04c398196de734d6d0faa7f2da02
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 18:15:37 -0300

    feat(Select): implement info transfer

--- Front-End/Block_02/Module_2/final_exercise/src/components/Select.jsx
+++ Front-End/Block_02/Module_2/final_exercise/src/components/Select.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 export default class Select extends Component {
   render() {
-    const { name, options } = this.props;
+    const { name, options, handleChange } = this.props;
 
     return (
-      <select id={name}>
+      <select id={name} onChange={handleChange}>
         {options.map((opt) => (
           <option key={opt} value={opt}>
             {opt}
