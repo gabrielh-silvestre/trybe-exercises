4c2f72b6ddfd3d912f9ce576b8f6380c46aff025
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 15:31:18 -0300

    feat(Form): import Input component and create main state

--- Front-End/Block_02/Module_2/class_exercises/src/components/Form.jsx
+++ Front-End/Block_02/Module_2/class_exercises/src/components/Form.jsx
@@ -1,18 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Input from './Input';
 
 export default class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userName: undefined,
+      userAge: undefined,
+      userTech: undefined,
+      userDesc: undefined,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState(() => ({
+      [target.name]: target.value,
+    }));
+  }
+
   render() {
     return (
       <form>
-        <input type="text" name="user-name" id="user-name" />
-        <input type="number" name="user-age" id="user-age" />
-        <select name="user-tech" id="user-tech">
+        <Input
+          inputConfig={{ type: 'text', name: 'userName', id: 'userName' }}
+          handleChange={this.handleChange}
+        />
+        <Input
+          inputConfig={{ type: 'number', name: 'userAge', id: 'userAge' }}
+          handleChange={this.handleChange}
+        />
+        <select name="userTech" id="userTech">
           <option value="react">React</option>
           <option value="angular">Angular</option>
           <option value="vue">Vue</option>
         </select>
-        <textarea name="user-desc" id="user-desc" cols="30" rows="10"></textarea>
+        <textarea name="userDesc" id="userDesc" cols="30" rows="10"></textarea>
       </form>
-    )
+    );
   }
 }
