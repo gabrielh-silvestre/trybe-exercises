943d2c5ff45b4d6c5b982a7057091e83fb645c1b
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 18:30:00 -0300

    feat(Input): add label

--- Front-End/Block_02/Module_2/final_exercise/src/components/Input.jsx
+++ Front-End/Block_02/Module_2/final_exercise/src/components/Input.jsx
@@ -4,6 +4,11 @@ export default class Input extends Component {
   render() {
     const { type, name, handleChange } = this.props;
 
-    return <input type={type} name={name} id={name} onChange={handleChange} />;
+    return (
+      <>
+        <label htmlFor={name}>{name}</label>
+        <input type={type} name={name} id={name} onChange={handleChange} />
+      </>
+    );
   }
 }
