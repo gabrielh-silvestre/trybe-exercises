dff6219f7213aaef99b8337de47385cfa3f9bab1
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 18:48:45 -0300

    feat(Form): implement handleChange function

--- Front-End/Block_02/Module_2/final_exercise/src/components/Form.jsx
+++ Front-End/Block_02/Module_2/final_exercise/src/components/Form.jsx
@@ -8,25 +8,69 @@ export default class Form extends Component {
     super(props);
 
     this.state = {
+      allStates: [
+        'AC',
+        'AL',
+        'AP',
+        'AM',
+        'BA',
+        'CE',
+        'DF',
+        'ES',
+        'GO',
+        'MA',
+        'MT',
+        'MS',
+        'MG',
+        'PA',
+        'PB',
+        'PR',
+        'PE',
+        'PI',
+        'RJ',
+        'RN',
+        'RS',
+        'RO',
+        'RR',
+        'SC',
+        'SP',
+        'SE',
+        'TO',
+      ],
       name: undefined,
       email: undefined,
       cpf: undefined,
       addres: undefined,
       city: undefined,
+      state: 'AC',
       home: 'house',
+      house: true,
+      appartment: false,
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { type, name, value, checked } }) {
+    this.setState({ [name]: value });
   }
 
   render() {
+    const { house, appartment, allStates } = this.state;
+
     return (
       <form>
         <Fieldset id="personal-data">
-          <Input type="text" name="name" handleChange="" />
-          <Input type="email" name="email" handleChange="" />
-          <Input type="number" name="cpf" handleChange="" />
-          <Input type="text" name="addres" handleChange="" />
-          <Input type="text" name="city" handleChange="" />
-          <Select name="state" handleChange="" options={[]} />
+          <Input type="text" name="name" handleChange={this.handleChange} />
+          <Input type="email" name="email" handleChange={this.handleChange} />
+          <Input type="number" name="cpf" handleChange={this.handleChange} />
+          <Input type="text" name="addres" handleChange={this.handleChange} />
+          <Input type="text" name="city" handleChange={this.handleChange} />
+          <Select
+            name="state"
+            handleChange={this.handleChange}
+            options={allStates}
+          />
           <div>
             <label htmlFor="house">House</label>
             <input
@@ -34,7 +78,8 @@ export default class Form extends Component {
               name="home"
               value="house"
               id="house"
-              checked={true}
+              checked={house}
+              onChange={this.handleChange}
             />
             <label htmlFor="appartment">Appartment</label>
             <input
@@ -42,6 +87,8 @@ export default class Form extends Component {
               name="home"
               value="appartment"
               id="appartment"
+              checked={appartment}
+              onChange={this.handleChange}
             />
           </div>
         </Fieldset>
