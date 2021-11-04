42342e2d3f92bf0cd1be05726f063c1bc156782b
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 18:14:19 -0300

    feat(TextArea): implement component props

--- Front-End/Block_02/Module_2/final_exercise/src/components/TextArea.jsx
+++ Front-End/Block_02/Module_2/final_exercise/src/components/TextArea.jsx
@@ -1,9 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class TextArea extends Component {
   render() {
+    const { name, handleChange } = this.props;
+
     return (
-      <textarea name="" id="" cols="30" rows="10"></textarea>
-    )
+      <textarea
+        name={name}
+        id={name}
+        cols="30"
+        rows="10"
+        onChange={handleChange}
+      ></textarea>
+    );
   }
 }
