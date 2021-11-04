ce18c076d9a5f2f295504d94e7591e964e3137e9
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 17:55:40 -0300

    feat: create TextArea component

---
+++ Front-End/Block_02/Module_2/final_exercise/src/components/TextArea.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class TextArea extends Component {
+  render() {
+    return (
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+    )
+  }
+}
