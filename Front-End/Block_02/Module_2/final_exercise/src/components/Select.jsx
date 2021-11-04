7fcd26e0c1ca4e70c9d72009c455f437270a9c8c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 18:30:56 -0300

    feat(Select): add label

--- Front-End/Block_02/Module_2/final_exercise/src/components/Select.jsx
+++ Front-End/Block_02/Module_2/final_exercise/src/components/Select.jsx
@@ -5,13 +5,16 @@ export default class Select extends Component {
     const { name, options, handleChange } = this.props;
 
     return (
-      <select id={name} onChange={handleChange}>
-        {options.map((opt) => (
-          <option key={opt} value={opt}>
-            {opt}
-          </option>
-        ))}
-      </select>
+      <>
+        <label htmlFor={name}>{name}</label>
+        <select id={name} onChange={handleChange}>
+          {options.map((opt) => (
+            <option key={opt} value={opt}>
+              {opt}
+            </option>
+          ))}
+        </select>
+      </>
     );
   }
 }
