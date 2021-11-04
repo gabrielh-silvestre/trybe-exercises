9669c89fbf15b31496e7822c537efc581769d6cf
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 20:02:12 -0300

    feat(Form): implement print resume

--- Front-End/Block_02/Module_2/final_exercise/src/components/Form.jsx
+++ Front-End/Block_02/Module_2/final_exercise/src/components/Form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Fieldset from './Fieldset';
 import Input from './Input';
+import Resume from './Resume';
 import Select from './Select';
 import TextArea from './TextArea';
 
@@ -9,35 +10,6 @@ export default class Form extends Component {
     super(props);
 
     this.state = {
-      allStates: [
-        'AC',
-        'AL',
-        'AP',
-        'AM',
-        'BA',
-        'CE',
-        'DF',
-        'ES',
-        'GO',
-        'MA',
-        'MT',
-        'MS',
-        'MG',
-        'PA',
-        'PB',
-        'PR',
-        'PE',
-        'PI',
-        'RJ',
-        'RN',
-        'RS',
-        'RO',
-        'RR',
-        'SC',
-        'SP',
-        'SE',
-        'TO',
-      ],
       name: undefined,
       email: undefined,
       cpf: undefined,
@@ -45,62 +17,105 @@ export default class Form extends Component {
       city: undefined,
       state: 'AC',
       home: 'house',
-      house: true,
-      appartment: false,
       resume: undefined,
       cargo: undefined,
       description: undefined,
+      display: 'none',
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.printResume = this.printResume.bind(this);
   }
 
   handleChange({ target: { name, value } }) {
-    this.setState({ [name]: value })
+    this.setState({ [name]: value });
+  }
+
+  printResume(event) {
+    event.preventDefault();
+
+    this.setState({ display: 'block' });
   }
 
   render() {
-    const { allStates } = this.state;
+    const allStates = [
+      'AC',
+      'AL',
+      'AP',
+      'AM',
+      'BA',
+      'CE',
+      'DF',
+      'ES',
+      'GO',
+      'MA',
+      'MT',
+      'MS',
+      'MG',
+      'PA',
+      'PB',
+      'PR',
+      'PE',
+      'PI',
+      'RJ',
+      'RN',
+      'RS',
+      'RO',
+      'RR',
+      'SC',
+      'SP',
+      'SE',
+      'TO',
+    ];
+    const { display } = this.state;
 
     return (
-      <form>
-        <Fieldset id="personal-data">
-          <Input type="text" name="name" handleChange={this.handleChange} />
-          <Input type="email" name="email" handleChange={this.handleChange} />
-          <Input type="number" name="cpf" handleChange={this.handleChange} />
-          <Input type="text" name="addres" handleChange={this.handleChange} />
-          <Input type="text" name="city" handleChange={this.handleChange} />
-          <Select
-            name="state"
-            handleChange={this.handleChange}
-            options={allStates}
-          />
-          <div>
-            <label htmlFor="house">House</label>
-            <input
-              type="radio"
-              name="home"
-              value="house"
-              id="house"
-              defaultChecked={true}
-              onChange={this.handleChange}
+      <>
+        <form>
+          <Fieldset id="personal-data">
+            <Input type="text" name="name" handleChange={this.handleChange} />
+            <Input type="email" name="email" handleChange={this.handleChange} />
+            <Input type="number" name="cpf" handleChange={this.handleChange} />
+            <Input type="text" name="addres" handleChange={this.handleChange} />
+            <Input type="text" name="city" handleChange={this.handleChange} />
+            <Select
+              name="state"
+              handleChange={this.handleChange}
+              options={allStates}
             />
-            <label htmlFor="appartment">Appartment</label>
-            <input
-              type="radio"
-              name="home"
-              value="appartment"
-              id="appartment"
-              onChange={this.handleChange}
+            <div>
+              <label htmlFor="house">House</label>
+              <input
+                type="radio"
+                name="home"
+                value="house"
+                id="house"
+                defaultChecked={true}
+                onChange={this.handleChange}
+              />
+              <label htmlFor="appartment">Appartment</label>
+              <input
+                type="radio"
+                name="home"
+                value="appartment"
+                id="appartment"
+                onChange={this.handleChange}
+              />
+            </div>
+          </Fieldset>
+          <Fieldset id="last-job">
+            <TextArea name="resume" handleChange={this.handleChange} />
+            <TextArea name="cargo" handleChange={this.handleChange} />
+            <Input
+              type="text"
+              name="description"
+              handleChange={this.handleChange}
             />
-          </div>
-        </Fieldset>
-        <Fieldset id="last-job">
-          <TextArea name="resume" handleChange={this.handleChange} />
-          <TextArea name="cargo" handleChange={this.handleChange} />
-          <Input type="text" name="description" handleChange={this.handleChange} />
-        </Fieldset>
-      </form>
+          </Fieldset>
+          <button onClick={this.printResume}>Print resume</button>
+        </form>
+        <Resume userInfo={this.state} display={display} />
+      </>
     );
   }
 }
