1de6ab0cc27c5a637b11a68a14b851a35fd9e15c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 22:53:20 -0300

    refactor(App.js): change Form component to Home page

--- Front-End/Block_02/Module_2/final_exercise/src/App.js
+++ Front-End/Block_02/Module_2/final_exercise/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Form from './components/Form';
+import Home from './page/Home';
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <Home />
     </div>
   );
 }
