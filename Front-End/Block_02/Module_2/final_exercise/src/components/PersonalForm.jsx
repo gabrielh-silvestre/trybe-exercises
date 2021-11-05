a20f47327fcfc7736af6ff24f52fa9c7018b9acd
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 22:55:03 -0300

    feat: create Home page and PersonalForm component

---
+++ Front-End/Block_02/Module_2/final_exercise/src/components/PersonalForm.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import Fieldset from './Fieldset';
+import Form from './Form';
+import Input from './Input';
+import Select from './Select';
+
+const brazilStates = [
+  { key: 'AC', value: 'Acre' },
+  { key: 'AL', value: 'Alagoas' },
+  { key: 'AP', value: 'Amapá' },
+  { key: 'AM', value: 'Amazonas' },
+  { key: 'BA', value: 'Bahia' },
+  { key: 'CE', value: 'Ceará' },
+  { key: 'DF', value: 'Distrito Federal' },
+  { key: 'ES', value: 'Espírito Santo' },
+  { key: 'GO', value: 'Goiás' },
+  { key: 'MA', value: 'Maranhão' },
+  { key: 'MT', value: 'Mato Grosso' },
+  { key: 'MS', value: 'Mato Grosso do Sul' },
+  { key: 'MG', value: 'Minas Gerais' },
+  { key: 'PA', value: 'Pará' },
+  { key: 'PB', value: 'Paraíba' },
+  { key: 'PR', value: 'Paraná' },
+  { key: 'PE', value: 'Pernambuco' },
+  { key: 'PI', value: 'Piauí' },
+  { key: 'RJ', value: 'Rio de Janeiro' },
+  { key: 'RN', value: 'Rio Grande do Norte' },
+  { key: 'RS', value: 'Rio Grande do Sul' },
+  { key: 'RO', value: 'Rondônia' },
+  { key: 'RR', value: 'Roraima' },
+  { key: 'SC', value: 'Santa Catarina' },
+  { key: 'SP', value: 'São Paulo' },
+  { key: 'SE', value: 'Sergipe' },
+  { key: 'TO', value: 'Tocantins' },
+];
+
+export default class PersonalForm extends Component {
+  render() {
+    const { handleChange } = this.props;
+
+    return (
+      <>
+        <Form>
+          <Fieldset>
+            <Input type="text" name="name" handleChange={handleChange} />
+            <Input type="text" name="email" handleChange={handleChange} />
+            <Input type="text" name="cpf" handleChange={handleChange} />
+            <Input type="text" name="address" handleChange={handleChange} />
+            <Input type="text" name="city" handleChange={handleChange} />
+            <Select name="state" options={brazilStates} handleChange={handleChange} />
+            <label htmlFor="house">Casa: </label>
+            <input type="radio" name="home" value="house" defaultChecked />
+            <label htmlFor="house">Apartamento: </label>
+            <input type="radio" name="home" value="appartment" />
+          </Fieldset>
+        </Form>
+      </>
+    );
+  }
+}
---
+++ Front-End/Block_02/Module_2/final_exercise/src/page/Home.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PersonalForm from '../components/PersonalForm';
+
+const initialState = {
+  name: '',
+  email: '',
+  cpf: '',
+  address: '',
+  city: '',
+  state: '',
+  home: '',
+  resume: '',
+  role: '',
+  roleDescription: '',
+  formError: {},
+  submitted: false,
+};
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    console.log(target);
+  }
+
+  render() {
+    return (
+      <div>
+        <PersonalForm handleChange={this.handleChange} />
+      </div>
+    );
+  }
+}
