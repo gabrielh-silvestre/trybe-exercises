dadf6a3626cd52f62335514da1752bc599d365fa
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-04 21:53:14 -0300

    refactor(Resume): build user resume strucuture

--- Front-End/Block_02/Module_2/final_exercise/src/components/Resume.jsx
+++ Front-End/Block_02/Module_2/final_exercise/src/components/Resume.jsx
@@ -2,20 +2,27 @@ import React, { Component } from 'react';
 
 export default class Resume extends Component {
   render() {
-    const { userInfo, display } = this.props;
+    const { name, email, cpf, addres, city, resume, cargo, description } =
+      this.props;
 
     return (
-      <div style={{ display: display }}>
-        {Object.entries(userInfo).map(([label, value], i) => {
-          return label !== 'display' ? (
-            <div key={i}>
-              <h3>{label}</h3>
-              <p>{value}</p>
-            </div>
-          ) : (
-            ''
-          );
-        })}
+      <div>
+        <h3>Name</h3>
+        <p>{name}</p>
+        <h3>Email</h3>
+        <p>{email}</p>
+        <h3>CPF</h3>
+        <p>{cpf}</p>
+        <h3>Addres</h3>
+        <p>{addres}</p>
+        <h3>City</h3>
+        <p>{city}</p>
+        <h3>Resume</h3>
+        <p>{resume}</p>
+        <h3>Cargo</h3>
+        <p>{cargo}</p>
+        <h3>Description</h3>
+        <p>{description}</p>
       </div>
     );
   }
