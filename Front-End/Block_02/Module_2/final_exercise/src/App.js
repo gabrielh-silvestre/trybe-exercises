a4f073b1479831a5109ff358fae89d6b23fe28b7
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-07 20:08:42 -0300

    chore: reset exercise

--- Front-End/Block_02/Module_2/final_exercise/src/App.js
+++ Front-End/Block_02/Module_2/final_exercise/src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Home from './page/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
     </div>
   );
 }
--- Front-End/Block_02/Module_2/final_exercise/src/components/Form.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Form extends Component {
-  render() {
-    const { children, handleClick } = this.props;
-
-    return <form onClick={handleClick}>{children}</form>;
-  }
-}
