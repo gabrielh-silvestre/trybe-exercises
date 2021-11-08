25cf0b9906d0a9e27d8b399292875f809716a15c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-07 22:34:00 -0300

    feat(Form): implement show feedback and clear form feature

--- Front-End/Block_02/Module_2/final_exercise/src/components/Form.jsx
+++ Front-End/Block_02/Module_2/final_exercise/src/components/Form.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Buttons from './Buttons';
 import FormFeedback from './FormFeedback';
 import PersonalFieldset from './PersonalFieldset';
 import ProffesionalFieldSet from './ProfessionalFieldset';
@@ -19,6 +20,7 @@ const initialForm = {
 export default function Form() {
   const [form, setForm] = useState(initialForm);
   const [hasError, setHasError] = useState(false);
+  const [showFeedback, setShowFeedback] = useState(false);
 
   useEffect(() => {
     Object.values(form).some((item) => item === '')
@@ -26,11 +28,24 @@ export default function Form() {
       : setHasError(false);
   }, [form]);
 
+  function clearForm(event) {
+    event.preventDefault();
+    setForm(initialForm);
+    setShowFeedback(false);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setShowFeedback(true);
+  }
+
   return (
     <form>
       <PersonalFieldset user={form} setPersonalForm={setForm} />
       <ProffesionalFieldSet user={form} setProfessionalForm={setForm} />
-      <FormFeedback user={form} />
+      <Buttons callback={handleSubmit} btnText="Enviar" disabled={hasError} />
+      <Buttons callback={clearForm} btnText="Limpar" />
+      {showFeedback ? <FormFeedback user={form} /> : ''}
     </form>
   );
 }
