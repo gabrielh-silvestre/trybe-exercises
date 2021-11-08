96b2ef522e11cdef76ccd10b4c48b7ed95dc7192
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-07 22:08:06 -0300

    feat(ProfessionalFieldset): implement dynamic state save

--- Front-End/Block_02/Module_2/final_exercise/src/components/ProfessionalFieldset.jsx
+++ Front-End/Block_02/Module_2/final_exercise/src/components/ProfessionalFieldset.jsx
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-export default function ProffesionalFieldSet() {
+export default function ProffesionalFieldSet(props) {
+  const professionalForm = props.user;
+  const { setProfessionalForm, setHasError } = props;
+
+  function handleChange({ target: { name, value } }) {
+    setProfessionalForm(() => ({ ...professionalForm, [name]: value }));
+  }
+
+  useEffect(() => {
+    Object.values(professionalForm).some((item) => item === '')
+      ? setHasError(true)
+      : setHasError(false);
+  });
+
   return (
     <fieldset>
       <label htmlFor="resume">
         Resumo do currículo:
-        <textarea name="resume" id="resume" cols="30" rows="10"></textarea>
+        <textarea
+          name="resume"
+          id="resume"
+          cols="30"
+          rows="10"
+          onChange={handleChange}
+        ></textarea>
       </label>
 
       <label htmlFor="role">
         Cargo:
-        <input type="text" name="role" id="role " />
+        <input type="text" name="role" id="role " onChange={handleChange} />
       </label>
 
       <label htmlFor="roleDescription">
         Descrição do cargo:
-        <input type="text" name="roleDescription" id="roleDescription" />
+        <input
+          type="text"
+          name="roleDescription"
+          id="roleDescription"
+          onChange={handleChange}
+        />
       </label>
     </fieldset>
-  )
+  );
 }
+
+ProffesionalFieldSet.propTypes = {
+  user: PropTypes.object.isRequired,
+  setHasError: PropTypes.func.isRequired,
+  setProfessionalForm: PropTypes.func.isRequired,
+};
