a5b19e057f2e1452d50c10cf54d1c6ae5dd56138
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-08 15:43:36 -0300

    feat(funcComponent): add coditional render

--- Front-End/Block_03/class_exercise/radom-user/src/components/functionalComponents/UserList.tsx
+++ Front-End/Block_03/class_exercise/radom-user/src/components/functionalComponents/UserList.tsx
@@ -17,18 +17,23 @@ interface UserCardProps {
 
 export default function UserList() {
   const [usersData, setUsersData] = useState<UserCardProps[]>([]);
+  const [loading, setLoading] = useState(false);
 
   async function fetchUsers() {
     const res = await fetch('https://randomuser.me/api/?results=20');
     const data = await res.json();
+    setLoading(false);
     setUsersData(data.results);
   }
 
   useEffect(() => {
+    setLoading(true);
     fetchUsers();
   }, []);
 
-  return (
+  return loading ? (
+    <p>Loading</p>
+  ) : (
     <article>
       {usersData.map((user) => (
         <UserCard user={user} />
