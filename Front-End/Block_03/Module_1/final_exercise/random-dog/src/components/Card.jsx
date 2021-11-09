908d61e990485c187bf74198ec9021329bcea790
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-09 19:58:34 -0300

    feat(Card): implement requisition button

--- Front-End/Block_03/Module_1/final_exercise/random-dog/src/components/Card.jsx
+++ Front-End/Block_03/Module_1/final_exercise/random-dog/src/components/Card.jsx
@@ -1,36 +1,38 @@
 import React, { useEffect, useState } from 'react';
+import Button from './Button';
 
 const dogBreedRegex = new RegExp(/(?<=breeds\/).*(?=\/)/g);
 
 export default function Card() {
   const [dogs, setDogs] = useState([]);
-
-  async function fetchDog() {
-    const res = await fetch('https://dog.ceo/api/breeds/image/random');
-    const data = await res.json();
-    setDogs([
-      {
-        ...dogs,
-        dog: dogContructor(data.message),
-      },
-    ]);
-  }
+  const [manyDogs, setManyDogs] = useState(1);
 
   function dogContructor(data) {
     return {
       dogImage: data,
-      dogBreed: data.match(dogBreedRegex),
+      dogBreed: data.match(dogBreedRegex)[0],
     };
+  }
+
+  async function fetchDog() {
+    const res = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await res.json();
+    setDogs([...dogs, dogContructor(data.message)]);
+  }
+
+  function handleClick() {
+    setManyDogs((prevState) => prevState + 1);
   }
 
   useEffect(() => {
     fetchDog();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [manyDogs]);
 
   return (
     <section>
-      {dogs.map(({ dog: { dogImage, dogBreed } }) => (
+      <Button callback={handleClick} btnText="Mais doguinhos" />
+      {dogs.map(({ dogImage, dogBreed }) => (
         <div key={dogImage}>
           <div>
             <img src={dogImage} alt={dogBreed} />
