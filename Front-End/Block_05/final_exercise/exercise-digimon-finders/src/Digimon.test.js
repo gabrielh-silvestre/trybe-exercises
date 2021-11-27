c728afb74511db53d4524e1073acfdcfe64b5b5e
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-26 22:05:50 -0300

    test: complete Digimon tests

--- Front-End/Block_05/final_exercise/exercise-digimon-finders/src/Digimon.test.js
+++ Front-End/Block_05/final_exercise/exercise-digimon-finders/src/Digimon.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Digimon from './Digimon';
+import App from './App';
+import data from './data';
+import userEvent from '@testing-library/user-event';
 
 describe('Teste da tela do Digimon', () => {
-  it('renders Digimon', async () => {
+  beforeEach(() => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue(data),
+    });
 
+    render(<App />);
+  });
+
+  it('01. Verify if digimon name is been render', () => {
+    const digimonName = screen.getByRole('heading', { level: 2 });
+
+    expect(digimonName).toBeInTheDocument();
+  });
+
+  it('02. Verify if digimon image is been render', async () => {
+    const input = screen.getByRole('textbox', { name: /digimon/i });    
+    const btn = screen.getByRole('button', { name: /search digimon/i });
+
+    userEvent.type(input, 'Koromon');
+    userEvent.click(btn);
+  
+    const digimonImage = await screen.findByRole('img');
+    expect(digimonImage).toBeInTheDocument();
   });
 });
