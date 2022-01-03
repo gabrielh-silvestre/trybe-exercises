1f6f7c73a8bb894e2dd07fd1d0c6e3eed83ab699
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-01-03 16:11:03 -0300

    feat: implement random color creation

--- Front-End/Block_06/final_exercise/ex-1/script.js
+++ Front-End/Block_06/final_exercise/ex-1/script.js
@@ -5,9 +5,11 @@ const ESTADO_INICIAL = {
 
 const NEXT_COLOR = 'NEXT_COLOR';
 const PREVIOUS_COLOR = 'PREVIOUS_COLOR';
+const ADD_COLOR = 'ADD_COLOR';
 
 const nextColor = { type: NEXT_COLOR };
 const prevColor = { type: PREVIOUS_COLOR };
+const addColor = (newColor) => ({ type: ADD_COLOR, newColor });
 
 function reducer(state = ESTADO_INICIAL, action) {
   switch (action.type) {
@@ -21,6 +23,12 @@ function reducer(state = ESTADO_INICIAL, action) {
         ...state,
         index: state.index > 0 ? state.index - 1 : state.colors.length - 1,
       };
+    case ADD_COLOR:
+      return {
+        ...state,
+        colors: [...state.colors, action.newColor],
+        index: state.colors.length,
+      };
     default:
       return state;
   }
@@ -31,8 +39,14 @@ const store = Redux.createStore(reducer);
 const elements = {
   prevButton: document.getElementById('previous'),
   nextButton: document.getElementById('next'),
+  randomButton: document.getElementById('random'),
   colorValue: document.getElementById('value'),
 };
+
+const randomNumber = () => Math.floor(Math.random() * 255);
+
+const generateRandomColor = () =>
+  `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
 
 const handleChangeColor = () => {
   const { prevButton, nextButton } = elements;
@@ -45,8 +59,17 @@ const handleChangeColor = () => {
   });
 };
 
+const handleRandomColor = () => {
+  const { randomButton } = elements;
+
+  randomButton.addEventListener('click', () => {
+    store.dispatch(addColor(generateRandomColor()));
+  });
+};
+
 window.onload = () => {
   handleChangeColor();
+  handleRandomColor();
   store.subscribe(() => {
     const { colors, index } = store.getState();
     elements.colorValue.innerText = colors[index];
