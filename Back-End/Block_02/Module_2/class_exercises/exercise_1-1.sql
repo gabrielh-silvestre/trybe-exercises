366a814e25271a40b1d84dad06d01e2d7a6c33d0
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-22 15:30:01 -0300

    feat: complete class exercise 1.1

---
+++ Back-End/Block_02/Module_2/class_exercises/exercise_1-1.sql
@@ -0,0 +1,11 @@
+SELECT 'Gabriel';
+
+SELECT 'Gabriel', 'Silvestre',
+'S. J. do Rio Preto', 21;
+
+SELECT 'Gabriel' AS `first-name`, 'Silvestre' AS `last-name`,
+'S. J. do Rio Preto' AS `city`, 21 AS `age`;
+
+SELECT 13 * 8;
+
+SELECT NOW() AS `Data Atual`;
