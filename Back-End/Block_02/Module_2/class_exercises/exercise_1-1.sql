6ce1be7f5d9df25ac1c3c274744afcf8c1e41161
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-22 15:46:53 -0300

    refactor: add exercise challenge

--- Back-End/Block_02/Module_2/class_exercises/exercise_1-1.sql
+++ Back-End/Block_02/Module_2/class_exercises/exercise_1-1.sql
@@ -1,11 +1,17 @@
-SELECT 'Gabriel';
+-- 1. Monte uma query que exiba seu nome na tela;
+-- 2. Monte uma query que exiba seu nome, sobrenome, cidade natal e idade na tela;
+-- 3. Monte uma query que, além de exibir todas as informações já mencionadas, identifique cada coluna usando o AS , que é chamado de alias na linguagem SQL ( alias é como um apelido no português);
+-- 4. Qual é o resultado de 13 * 8 ? Descubra usando apenas o SELECT ;
+-- 5. Monte uma query que exiba a data e hora atuais. Dê a essa coluna o nome "Data Atual".
 
-SELECT 'Gabriel', 'Silvestre',
+/* 1 */ SELECT 'Gabriel';
+
+/* 2 */ SELECT 'Gabriel', 'Silvestre',
 'S. J. do Rio Preto', 21;
 
-SELECT 'Gabriel' AS `first-name`, 'Silvestre' AS `last-name`,
+/* 3 */ SELECT 'Gabriel' AS `first-name`, 'Silvestre' AS `last-name`,
 'S. J. do Rio Preto' AS `city`, 21 AS `age`;
 
-SELECT 13 * 8;
+/* 4 */ SELECT 13 * 8;
 
-SELECT NOW() AS `Data Atual`;
+/* 5 */ SELECT NOW() AS `Data Atual`;
--- Back-End/Block_02/Module_2/class_exercises/exercise_1-2.sql
+++ Back-End/Block_02/Module_2/class_exercises/exercise_1-2.sql
@@ -1,12 +1,18 @@
-SELECT * FROM sakila.city;
+-- 1. Escreva uma query que selecione todas as colunas da tabela city ;
+-- 2. Escreva uma query que exiba apenas as colunas first_name e last_name da tabela customer ;
+-- 3. Escreva uma query que exiba todas as colunas da tabela rental ;
+-- 4. Escreva uma query que exiba o título, a descrição e a data de lançamento dos filmes registrados na tabela film ;
+-- 5. Utilize o SELECT para explorar todas as tabelas do banco de dados.
 
-SELECT first_name, last_name FROM sakila.customer;
+/* 1 */ SELECT * FROM sakila.city;
 
-SELECT * FROM sakila.rental;
+/* 2 */ SELECT first_name, last_name FROM sakila.customer;
 
-SELECT title, `description`, release_year
+/* 3 */ SELECT * FROM sakila.rental;
+
+/* 4 */ SELECT title, `description`, release_year
 FROM sakila.film;
 
-SELECT * FROM sakila;
+/* 5 */ SELECT * FROM sakila;
 
-SHOW TABLES;
+/* 6 */ SHOW TABLES;
