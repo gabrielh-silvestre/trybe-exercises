675c882a657f9f6a8250cd66f104f5cd79fd5744
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-02-22 15:42:18 -0300

    feat: complete class exercise 2.1

---
+++ Back-End/Block_02/Module_2/class_exercises/exercise_2-1.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(title, ' ', release_year) AS `Lançamento do Filme`
+FROM sakila.film;
+
+SELECT CONCAT(title, ' ', rating) AS `Classificação`
+FROM sakila.film;
+
+SELECT CONCAT(`address`, ' ', district) AS `Endereço`
+FROM sakila.address;
