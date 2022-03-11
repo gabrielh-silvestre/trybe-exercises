376e92e126aa66d275d004245441d73d66643a6a
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-11 16:06:08 -0300

    feat(day-22.1): implement interactive in IMC script
    - use dinamyc data to calculate IMC

--- Back-End/Block_04/Module_1/imc.js
+++ Back-End/Block_04/Module_1/imc.js
@@ -1,5 +1,19 @@
+const readlineSync = require('readline-sync');
+
 const calcIMC = (height = 177, weight = 82) => {
-  console.log((weight / ((height / 100) ** 2)).toFixed(2));
+  console.log('Seu IMC é de ' + (weight / ((height / 100) ** 2)).toFixed(2));
 }
 
-calcIMC();
+const getUserInfo = () => {
+  const weight = readlineSync.questionFloat('Quanto você pesa? ');
+  const height = readlineSync.questionInt('Qual sua altura em cm? ');
+
+  return {
+    weight,
+    height,
+  }
+};
+
+const { weight, height } = getUserInfo();
+
+calcIMC(height, weight);
