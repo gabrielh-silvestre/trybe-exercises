3e50a642b6dbb64394fbbbae6ea7ad69ac2a7ddf
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-11 17:03:34 -0300

    refactor(day-22.1): change guess game message order
    - change message to better experience

--- Back-End/Block_04/Module_1/guessGame.js
+++ Back-End/Block_04/Module_1/guessGame.js
@@ -10,13 +10,13 @@ console.log('Advinhe o número de 0 até 10');
 readlineSync.promptLoop((input) => {
   tryNumber += 1;
 
-  if (MAX_TRY - tryNumber !== 0) {
-    console.log(`Você tem ${MAX_TRY - tryNumber} tentativas restantes.`);
-  }
-
   if (randomNumber === Number(input)) {
     console.log(`Você acertou em ${tryNumber} tentativas, parabéns!`);
     return randomNumber === Number(input);
+  }
+
+  if (MAX_TRY - tryNumber !== 0) {
+    console.log(`Você tem ${MAX_TRY - tryNumber} tentativas restantes.`);
   }
 
   if (tryNumber === MAX_TRY) {
