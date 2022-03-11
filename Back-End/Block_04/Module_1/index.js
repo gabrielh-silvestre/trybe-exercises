a0057de2438e8f03c74f890f7513a84c3a1c7f2a
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-11 17:22:19 -0300

    feat(day-22.1) add factorial calculator in menu

--- Back-End/Block_04/Module_1/index.js
+++ Back-End/Block_04/Module_1/index.js
@@ -4,6 +4,7 @@ const services = [
   'Calculadora IMC',
   'Calculadora de Velocidade Média',
   'Jogo de Advinhação',
+  'Calculadora de Fatorial',
 ];
 
 const selectedService = readlineSync.keyInSelect(services, 'O que quer fazer?');
@@ -12,6 +13,7 @@ const servicesFiles = [
   () => require('./imc'),
   () => require('./speed'),
   () => require('./guessGame'),
+  () => require('./factorial'),
 ];
 
 console.log(`Você selecionou ${services[selectedService]}`);
