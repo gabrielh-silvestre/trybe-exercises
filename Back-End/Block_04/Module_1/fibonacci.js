a47168b30730d668705170bfe7dd747a7f484fe3
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-11 20:02:39 -0300

    refactor(fibonacci): export object with functions, instead of all file

--- Back-End/Block_04/Module_1/fibonacci.js
+++ Back-End/Block_04/Module_1/fibonacci.js
@@ -1,11 +1,10 @@
 const readlineSync = require('readline-sync');
 
-const fibonacciLimit = readlineSync.questionInt('Quantos números da sequência de Fibonacci você quer? ');
-
-const initialFibonnaci = [0, 1, 1];
-const INITIAL_LENGTH = initialFibonnaci.length;
+const getFibonacciLimit = () =>  readlineSync.questionInt('Quantos números da sequência de Fibonacci você quer? ');
 
 const getFibonnaciSequence = (sequenceLength) => {
+  const initialFibonnaci = [0, 1, 1];
+  const INITIAL_LENGTH = initialFibonnaci.length;
   let tempArr = [...initialFibonnaci];
 
   for (let i = 1; i <= (sequenceLength - INITIAL_LENGTH); i += 1) {
@@ -15,6 +14,15 @@ const getFibonnaciSequence = (sequenceLength) => {
   return tempArr;
 }
 
-const fibonacciSequence = getFibonnaciSequence(fibonacciLimit);
+const executeServiceFibonacci = () => {
+  const fibonacciLimit = getFibonacciLimit();
+  const fibonacciSequence = getFibonnaciSequence(fibonacciLimit);
+  console.log(`A seguência de Fibonacci até ${fibonacciLimit} é: \n ${fibonacciSequence}`);
 
-console.log(`A seguência de Fibonacci até ${fibonacciLimit} é: \n ${fibonacciSequence}`);
+}
+
+module.exports = {
+  getFibonacciLimit,
+  getFibonnaciSequence,
+  executeServiceFibonacci,
+}
