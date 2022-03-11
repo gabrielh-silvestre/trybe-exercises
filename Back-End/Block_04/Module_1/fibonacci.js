d62f0b1b9f33b1a22473ce9834712ec4d1cfc699
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-11 20:06:37 -0300

    refactor(fibonacci): remove 0 from fibonacci result

--- Back-End/Block_04/Module_1/fibonacci.js
+++ Back-End/Block_04/Module_1/fibonacci.js
@@ -3,11 +3,11 @@ const readlineSync = require('readline-sync');
 const getFibonacciLimit = () =>  readlineSync.questionInt('Quantos números da sequência de Fibonacci você quer? ');
 
 const getFibonnaciSequence = (sequenceLength) => {
-  const initialFibonnaci = [0, 1, 1];
+  const initialFibonnaci = [1, 1];
   const INITIAL_LENGTH = initialFibonnaci.length;
   let tempArr = [...initialFibonnaci];
 
-  for (let i = 1; i <= (sequenceLength - INITIAL_LENGTH); i += 1) {
+  for (let i = 0; i < (sequenceLength - INITIAL_LENGTH); i += 1) {
     tempArr.push(tempArr[i] + tempArr[i + 1]);
   }
 
