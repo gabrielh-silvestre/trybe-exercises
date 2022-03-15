cf513acfc851cfe0920919caad0bd1dd499a97cc
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-14 22:55:53 -0300

    feat(day-22.2): resolve exercise 5
    - ignore created files
    - use Promisse.all to create multiple files

---
+++ Back-End/Block_04/Module_2/exercise_5/exercise-5.js
@@ -0,0 +1,26 @@
+const fs = require('fs/promises');
+
+const STRING_ARR = ['Finalmente', 'estou', 'usando', 'Promise.all', '!!!'];
+
+const createMultFiles = async () => {
+  const createFilesArr = STRING_ARR.map((str, i) =>
+    fs.writeFile(`./exercise_5/file${i + 1}.txt`, str)
+  );
+
+  return Promise.all(createFilesArr);
+};
+
+const centerInformation = async () => {
+  await createMultFiles();
+  const tempArr = [];
+
+  for (let i = 1; i <= STRING_ARR.length; i += 1) {
+    tempArr.push(fs.readFile(`./exercise_5/file${i}.txt`));
+  }
+
+  const solvedArr = await Promise.all(tempArr);
+
+  await fs.writeFile('./exercise_5/fileAll.txt', solvedArr.join(' '));
+}
+
+centerInformation();
