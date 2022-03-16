dc276b3862e8834ebf2c0e04595523f773d825c8
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-16 16:49:57 -0300

    feat(day-22.4/services): create new service to find one simpson
    - implement service on /simpsons/:id route
    - create another exception

--- Back-End/Block_04/Module_4/exercise_2/src/routes/simpsonsRoute.js
+++ Back-End/Block_04/Module_4/exercise_2/src/routes/simpsonsRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { listAll } = require('../services/simpsonsServices');
+const { listAll, findOne } = require('../services/simpsonsServices');
+
 const simpsonsRoute = express.Router();
 
 simpsonsRoute.get('/', async (_req, res) => {
@@ -12,6 +13,17 @@ simpsonsRoute.get('/', async (_req, res) => {
   return res.status(200).json(allSimpsons);
 });
 
+simpsonsRoute.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const simpson = await findOne(id);
+
+  if (simpson.message) {
+    return res.status(500).json({ message: simpson.message });
+  }
+
+  return res.status(200).json(simpson);
+});
+
 module.exports = {
   simpsonsRoute,
-}
+};
--- Back-End/Block_04/Module_4/exercise_2/src/services/simpsonsServices.js
+++ Back-End/Block_04/Module_4/exercise_2/src/services/simpsonsServices.js
@@ -1,4 +1,4 @@
-const { getSimpsons } = require("../model/simpsonsModel");
+const { getSimpsons } = require('../model/simpsonsModel');
 
 const parseJSON = (str) => JSON.parse(str);
 
@@ -6,10 +6,24 @@ const listAll = async () => {
   try {
     return parseJSON(await getSimpsons());
   } catch (err) {
-    return { message: err.message }
+    return { message: err.message };
   }
-}
+};
+
+const findOne = async (id) => {
+  try {
+    const simpsons = parseJSON(await getSimpsons());
+    return (
+      simpsons.find((s) => s.id === id) || {
+        message: 'Character does not exist!',
+      }
+    );
+  } catch (err) {
+    return { message: err.message };
+  }
+};
 
 module.exports = {
   listAll,
-}
+  findOne,
+};
