5b68fc739e6b4783ff1bf366176e727040f1bb64
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-17 18:00:22 -0300

    feat(day-22.5/controllers): create btc controller
    - implement controller on btc route
    - implement btc route on file entrypoint

--- Back-End/Block_04/Module_5/src/index.js
+++ Back-End/Block_04/Module_5/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const { userRouter } = require('./routes/userRoute');
+const { btcRouter } = require('./routes/btcRoute');
+
+const { auth } = require('./middlewares');
 
 const PORT = 3001;
 const api = express();
@@ -8,6 +11,7 @@ const api = express();
 api.use(express.json());
 
 api.use('/user', userRouter);
+api.use('/btc', auth, btcRouter);
 
 api.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
