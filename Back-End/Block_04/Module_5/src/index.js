7fa47517e702eb2ad622118d8077211f76f759f9
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-17 17:02:01 -0300

    feat(day-22.4/index.js): apply user router on entrypoint file

--- Back-End/Block_04/Module_5/src/index.js
+++ Back-End/Block_04/Module_5/src/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
+const { userRouter } = require('./routes/userRoute');
+
 const PORT = 3001;
 const api = express();
+
+api.use(express.json());
+
+api.use('/user', userRouter);
 
 api.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
