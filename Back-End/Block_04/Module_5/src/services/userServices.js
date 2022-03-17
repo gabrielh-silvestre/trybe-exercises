4f0db0a15a42e030b3ac7eb98f70d49d011d8234
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-17 16:55:31 -0300

    feat(day-22.5/services): create services for user
    - valid data
    - return status and message
    - rules: username > 3 letters; password BETWEEN 4 AND 8; email HAVE @

---
+++ Back-End/Block_04/Module_5/src/services/userServices.js
@@ -0,0 +1,50 @@
+const isMoreThenMinLength = (string, minLength) => string.length >= minLength;
+const isLessThenMaxLength = (string, maxLength) => string.length <= maxLength;
+const haveFormat = (string, regexFormat) => regexFormat.test(string);
+
+const isUsernameValid = (username) => {
+  const MIN_USERNAME_LENGTH = 3;
+
+  return isMoreThenMinLength(username, MIN_USERNAME_LENGTH);
+};
+
+const isPasswordValid = (password) => {
+  const MIN_PASSWORD_LENGTH = 4;
+  const MAX_PASSWORD_LENGTH = 8;
+
+  return (
+    isMoreThenMinLength(password, MIN_PASSWORD_LENGTH) &&
+    isLessThenMaxLength(password, MAX_PASSWORD_LENGTH)
+  );
+};
+
+const isEmailValid = (email) => {
+  const EMAIL_REGEX = new RegExp(
+    /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+    'gm'
+  );
+
+  return haveFormat(email, EMAIL_REGEX);
+};
+
+const validData = ({ username, email, password }) => {
+  const FAIL_STATUS = 400;
+  const SUCCES_STATUS = 201;
+
+  switch (false) {
+    case username || email || password:
+      return { status: FAIL_STATUS, message: 'Invalid data' };
+    case isUsernameValid(username):
+      return { status: FAIL_STATUS, message: 'Invalid username' };
+    case isPasswordValid(password):
+      return { status: FAIL_STATUS, message: 'Invalid password' };
+    case isEmailValid(email):
+      return { status: FAIL_STATUS, message: 'Invalid email' };
+    default:
+      return { status: SUCCES_STATUS, message: 'User created' };
+  }
+};
+
+module.exports = {
+  validData,
+};
