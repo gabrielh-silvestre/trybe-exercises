eaff05c73c983c9a1fde46da7de0f45d02fb2063
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-17 17:22:31 -0300

    feat(day-22.5/controllers): create login controller in userController

--- Back-End/Block_04/Module_5/src/controllers/userController.js
+++ Back-End/Block_04/Module_5/src/controllers/userController.js
@@ -1,12 +1,20 @@
-const { validData } = require('../services/userServices');
+const { validRegister, validLogin } = require('../services/userServices');
 
 const registerUser = (req, res) => {
   const { username, email, password } = req.body;
-  const { status, message } = validData({ username, email, password });
+  const { status, message } = validRegister({ username, email, password });
 
   return res.status(status).json({ message });
 };
 
+const loginUser = (req, res) => {
+  const { email, password } = req.body;
+  const { status, message, token } = validLogin({ email, password });
+
+  return res.status(status).json(message ? { message } : { token });
+};
+
 module.exports = {
   registerUser,
+  loginUser,
 };
