d7ee082518c2bebaa672dea542da5e184f3b958d
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-24 17:51:41 -0300

    feat(routes/userRoute): implment find all users route

--- Back-End/Block_05/Module_1/src/routes/userRoute.js
+++ Back-End/Block_05/Module_1/src/routes/userRoute.js
@@ -3,9 +3,13 @@ const express = require('express');
 const {
   createUserController,
 } = require('../modules/users/useCases/createUser/createUserController');
+const {
+  findAllUsersController,
+} = require('../modules/users/useCases/findAllUsers/findAllUsersController');
 
 const userRoute = express.Router();
 
 userRoute.post('/', createUserController);
+userRoute.get('/', findAllUsersController);
 
 module.exports = { userRoute };
