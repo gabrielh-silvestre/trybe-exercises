5af0a6fee85dcda9ddfb7e6113005a5167e582b5
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-25 00:16:21 -0300

    refactor(users/useCases): remove error treat and data validation

--- Back-End/Block_05/Module_1/src/modules/users/useCases/createUser/createUserController.js
+++ Back-End/Block_05/Module_1/src/modules/users/useCases/createUser/createUserController.js
@@ -1,16 +1,15 @@
 const { createUserUseCase } = require('./createUserUseCase');
 
-const createUserController = async (req, res) => {
+const createUserController = async (req, res, next) => {
   const { firstName, lastName, email, password } = req.body;
   const newUser = { firstName, lastName, email, password };
 
-  const { code, message, data } = await createUserUseCase(newUser);
-
-  if (message) {
-    return res.status(code).json({ message });
+  try {
+    const { code, data } = await createUserUseCase(newUser);
+    return res.status(code).json(data);
+  } catch (err) {
+    next(err);
   }
-
-  return res.status(code).json(data);
 };
 
 module.exports = { createUserController };
