019a3833122fea12bce091b543e2f9062efba483
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-06 17:47:00 -0300

    build(npm): create structure for exercise of day 24.1
    - use express, mysql, sequelize and docker
    - use environment variables for mysql
    - create first migration

---
+++ Back-End/Block_06/Module_1/src/orm/migrations/20220406203143-create-books.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('books', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('books');
+  },
+};
