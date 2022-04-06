f70de461abd32c2b4a7be61a34db8763ab8234da
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-06 19:15:43 -0300

    feat(orm/models): create books model
    - correct config file extension on index

---
+++ Back-End/Block_06/Module_1/src/orm/models/books.js
@@ -0,0 +1,30 @@
+const Books = (sequelize, DataTypes) => {
+  return sequelize.define('Book', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    pageQuantity: {
+      type: DataTypes.INTEGER,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  });
+};
+
+module.exports = Books;
